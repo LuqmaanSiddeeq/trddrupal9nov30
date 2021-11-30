dba5b99740891a8db346cc4a7b14f7d25e06e668
LuqmaanSiddeeqLK <43641228+LuqmaanSiddeeq@users.noreply.github.com>
2021-11-30 12:29:15 +0530

    Update settings.php

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -815,4 +815,4 @@ $databases['default']['default'] = array (
 $settings['config_sync_directory'] = 'sites/default/files/config_q6cicIz5nkMiSdJkhoohksyAjeId5CpfNNKMwlSeR9rUM1R68k-Hau6IDaHn5lXAuWn41WIFzQ/sync';
 
 $config['system.logging']['error_level'] = 'verbose';
-$settings['update_free_access'];
+$settings['update_free_access']= TRUE;
